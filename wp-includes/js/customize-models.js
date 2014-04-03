1a5b4825a42d8aeade316e53299a174095d6be00
David A. Kennedy <me@davidakennedy.com>
2014-04-02 20:29:47 -0400

    Apply 21785/a11y-ui-improvements.diff.

--- wp-includes/js/customize-models.js
+++ wp-includes/js/customize-models.js
@@ -26,7 +26,7 @@
 					thumbnail_url: ''
 				},
 				choice: '',
-				hidden: false,
+				selected: false,
 				random: false
 			};
 		},
@@ -160,10 +160,14 @@
 					elt.defaultName = index;
 				}
 
+				if (typeof elt.timestamp === 'undefined') {
+					elt.timestamp = 0;
+				}
+
 				this.add({
 					header: elt,
 					choice: elt.url.split('/').pop(),
-					hidden: current === elt.url.replace(/^https?:\/\//, '')
+					selected: current === elt.url.replace(/^https?:\/\//, '')
 				}, { silent: true });
 			}, this);
 
@@ -191,7 +195,7 @@
 				},
 				choice: randomChoice,
 				random: true,
-				hidden: isRandomSameType
+				selected: isRandomSameType
 			});
 		},
 
@@ -200,16 +204,16 @@
 		},
 
 		shouldHideTitle: function() {
-			return _.every(this.pluck('hidden'));
+			return this.size() < 2;
 		},
 
 		setImage: function(model) {
 			this.each(function(m) {
-				m.set('hidden', false);
+				m.set('selected', false);
 			});
 
 			if (model) {
-				model.set('hidden', true);
+				model.set('selected', true);
 				// Bump images to top except for special "Randomize" images
 				if (!model.get('random')) {
 					model.get('header').timestamp = _.now();
@@ -220,13 +224,8 @@
 
 		removeImage: function() {
 			this.each(function(m) {
-				m.set('hidden', false);
+				m.set('selected', false);
 			});
-		},
-
-		shown: function() {
-			var filtered = this.where({ hidden: false });
-			return new api.HeaderTool.ChoiceList( filtered );
 		}
 	});
 
